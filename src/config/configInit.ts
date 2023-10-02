622ab3ff22e81fb356efee3e8700e78bd922d7e4
chuantt <chuan123789@gmail.com>
2023-10-02 14:22:56 +0700

    update images default

--- src/config/configInit.ts
+++ src/config/configInit.ts
@@ -1,1 +1,4 @@
-export const defaultAvatar = [['female_1.jpg'], ['man_1.jpg']]
+export const defaultAvatar = [
+  ['female_1.png', 'female_2.png', 'female_3.png'],
+  ['male_1.png', 'male_2.png', 'male_3.png']
+]
